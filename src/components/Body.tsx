ab8b7b94342d8e516411c154dc77150b459e17a0
Baris Bilgin <brokoliweb@gmail.com>
2023-06-06 12:06:45 +0300

    always show last exhibition

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -5,24 +5,26 @@ type BodyProps = {
 };
 
 function Body({ language }: BodyProps) {
+  const index = exhibitions.length - 1;
+
   return (
     <div>
-      {exhibitions.map((exhibition) => (
-        <div>
-          <h1>
-            {language === "en-US" ? exhibition.titleEN : exhibition.titleTR}
-          </h1>
-          <span>{exhibition.date}</span>
-          <div
-            dangerouslySetInnerHTML={{
-              __html:
-                language === "en-US"
-                  ? exhibition.contentEN
-                  : exhibition.contentTR,
-            }}
-          />
-        </div>
-      ))}
+      <div>
+        <h1>
+          {language === "en-US"
+            ? exhibitions[index].titleEN
+            : exhibitions[index].titleTR}
+        </h1>
+        <span>{exhibitions[index].date}</span>
+        <div
+          dangerouslySetInnerHTML={{
+            __html:
+              language === "en-US"
+                ? exhibitions[index].contentEN
+                : exhibitions[index].contentTR,
+          }}
+        />
+      </div>
     </div>
   );
 }
