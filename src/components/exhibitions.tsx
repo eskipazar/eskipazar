8207822bc57ec2042e09f87ecb204eaa376bea13
Baris Bilgin <brokoliweb@gmail.com>
2023-06-06 12:51:22 +0300

    fix styles

--- src/components/exhibitions.tsx
+++ src/components/exhibitions.tsx
@@ -14,9 +14,9 @@ export const exhibitions: Exhibition[] = [
     contentEN: `<p>
       <img
         src="../src/images/dalgakiran.jpg"
-        className="w-full h-auto"
+        class="w-full h-auto"
       />
-      <hr class="poster-seperator" />
+      <hr class="border-dashed border-2 m-1 w-4 mx-auto my-2" />
       Eski Pazar is excited to present its first exhibition "Breakwater"... <br /><br />
 
       Eski Pazar envisages to become a playground, open to collective use, bringing people together. It embarked upon its quest by paying a visit to the local artists. With an inductive approach, The Wavebreaker emerged through the common theme of fluidity/fixity, which strikingly appeared in all of the artists' work. <br />  <br />
@@ -25,9 +25,9 @@ export const exhibitions: Exhibition[] = [
     contentTR: `<p>
       <img
       src="../src/images/dalgakiran.jpg"
-      className="w-full h-auto"
+      class="w-full h-auto"
       />
-      <hr class="poster-seperator" />
+      <hr class="border-dashed border-2 m-1 w-4 mx-auto my-2"  />
       Eski Pazar ilk sergisi “Dalgakıran”ı heyecanla sunar... <br /><br />
 
       Kolektif kullanıma açık, insanları bir araya getirmek üzere bir serbest oyun alanı olma hayali kuran Eski Pazar, kentin sanatçılarını ziyaret etmekle işe başladı. Dalgakıran, tümevarımsal bir biçimde, sanatçıların işlerinde dikkat çeken akışkanlık/sabitlik ortak teması dolayımıyla kendini buldu.  <br />  <br />
