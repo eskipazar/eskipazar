9a83906beed53cf9dc1d8a73001543e854960c89
Baris Bilgin <brokoliweb@gmail.com>
2023-08-08 15:06:31 +0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   build: {
     outDir: "dist", // Specify the output directory for the production build
-    sourcemap: true, // Generate sourcemaps for debuggin
+    sourcemap: true, // Generate sourcemaps for debugging
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
   },
 });
